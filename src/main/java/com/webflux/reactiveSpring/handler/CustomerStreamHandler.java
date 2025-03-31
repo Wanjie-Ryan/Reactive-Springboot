a4b17932d428429d3a9943170f671ece0eca51cd
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-03-31 13:01:16 +0300

    reactive spring boot (mono and flux)

--- src/main/java/com/webflux/reactiveSpring/handler/CustomerStreamHandler.java
+++ src/main/java/com/webflux/reactiveSpring/handler/CustomerStreamHandler.java
@@ -3,6 +3,7 @@ package com.webflux.reactiveSpring.handler;
 import com.webflux.reactiveSpring.dao.CustomerDao;
 import com.webflux.reactiveSpring.dto.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -17,6 +18,8 @@ public class CustomerStreamHandler {
 
     public Mono<ServerResponse> getCustomers(ServerRequest request) {
         Flux<Customer> customerLoadData = customerDao.getCustomersStream();
-        return ServerResponse.ok().body(customerLoadData, Customer.class);
+        // the content type is set so that data is sent in event/stream wise, and not like a normal json object
+        // this enables a continuous stream of data to the client as the data becomes available, rather than sending all data at once like a normal JSON response.
+        return ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(customerLoadData, Customer.class);
     }
 }
